2cb97e2927ed15ee89ef347fc995254355bb4c04
Luiza-Issi-Portfolio <issi.luiza@hotmail.com>
2025-05-19 15:07:48 -0300

    mudanca nos nomes dos produtos e adicao de imagens

--- Ecommerce/data.js
+++ Ecommerce/data.js
@@ -1,8 +1,8 @@
 // Simulação de produtos
 const produtos = [
-  { id: 1, nome: "Camiseta Básica", preco: 59.90, imagem: "https://www.markamania.es/151867-large_default/camisetas-roly-beagle-blancas.jpg" },
-  { id: 2, nome: "Tênis Esportivo", preco: 299.90, imagem: "https://via.placeholder.com/150" },
-  { id: 3, nome: "Jaqueta Jeans", preco: 179.90, imagem: "https://via.placeholder.com/150" }
+  { id: 1, nome: "Camiseta manga curta flor", preco: 99.90, imagem: "https://static.bershka.net/assets/public/8566/6830/3dd14132b266/b193761d10c7/07567071251-p/07567071251-p.jpg?ts=1746521606435&w=750" },
+  { id: 2, nome: "Camisa manga curta print", preco: 99.90, imagem: "https://static.bershka.net/assets/public/5d58/5b41/b85d4e2cbad7/4e7c1ca15103/02417093251-p/02417093251-p.jpg?ts=1744726252027&w=750" },
+  { id: 3, nome: "Camiseta manga curta tailândia", preco: 99.90, imagem: "https://static.bershka.net/assets/public/9635/996a/6f62450586be/be9a19cc97de/02358152251-p/02358152251-p.jpg?ts=1737476685619&w=750" }
 ];
 
 // Simulação de pedidos (salva no localStorage)
